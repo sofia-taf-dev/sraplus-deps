1eddf008b94bcddd26959372ef13e2012bcd6e87
Arni Magnusson <thisisarni@gmail.com>
2025-03-14 16:03:09 +1100

    Parse imports

--- data.R
+++ data.R
@@ -1,9 +1,20 @@
 # Preprocess data, write TAF data tables
 
-# Before:
-# After:
+# Before: DESCRIPTION (boot/data)
+# After:  imports.csv (data)
 
 library(TAF)
 
 mkdir("data")
 
+# Read Imports entry from DESCRIPTION file
+imports <- read.dcf("boot/data/DESCRIPTION", fields="Imports")
+
+# Split long string into package names
+imports <- unlist(strsplit(imports, "\\n"))
+imports <- sub(",", "", imports)    # remove comma
+imports <- sub(" .*", "", imports)  # remove version number
+imports <- data.frame(package=imports)
+
+# Write TAF table
+write.taf(imports, dir="data")
