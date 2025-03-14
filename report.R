dfb2dd149fc64f9bea92dfe2b91d81fe56b525d4
Arni Magnusson <thisisarni@gmail.com>
2025-03-14 16:06:20 +1100

    Plot dependency count

--- report.R
+++ report.R
@@ -1,9 +1,20 @@
 # Prepare plots and tables for report
 
-# Before:
-# After:
+# Before: count.csv (output)
+# After:  count.png (report)
 
 library(TAF)
 
 mkdir("report")
 
+# Read results
+count <- read.taf("output/count.csv")
+n <- count$Dependencies[count$Package == "sraplus"]
+
+# Plot
+main <- paste0("Dependencies of sraplus (n = ", n, ")")
+taf.png("count", 1200, 1600)
+par(plt=c(0.26,0.90,0.11,0.90))
+barplot(count$Dependencies, names=count$Package, horiz=TRUE, las=1,
+        main=main, xlab="Dependencies", col="burlywood")
+dev.off()
