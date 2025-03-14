0fb654fa481299c8c113688b1634e7a583feb2d5
Arni Magnusson <thisisarni@gmail.com>
2025-03-14 16:05:48 +1100

    Analyze dependencies

--- model.R
+++ model.R
@@ -1,9 +1,21 @@
 # Run analysis, write model results
 
-# Before:
-# After:
+# Before: imports.csv (data), pdeps.R (boot/software)
+# After:  dependencies.RData (model)
 
 library(TAF)
+library(tools)
+source("boot/software/pdeps.R")
+options(repos=c(CRAN="https://cloud.r-project.org"))
 
 mkdir("model")
 
+# Read imported package names
+imports <- read.taf("data/imports.csv")$package
+
+# Analyze primary and secondary dependencies
+primary <- sort(unique(imports))
+secondary.list <- pdeps(imports)
+
+# Write results
+save(primary, secondary.list, file="model/dependencies.RData")
